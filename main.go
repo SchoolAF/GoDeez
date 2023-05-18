5ddf138cadfca384c12ebee6712f0411407d6846
HeroBuxx <me@herobuxx.me>
2023-05-18 13:26:15 +0700

    [TREEWIDE] GoDeez: Initial import working backend and basic frontend
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+type Track struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Artist struct {
+		Name string `json:"name"`
+	} `json:"artist"`
+	Album struct {
+		Title    string `json:"title"`
+		CoverURL string `json:"cover_medium"`
+	} `json:"album"`
+	Duration   int    `json:"duration"`
+	PreviewURL string `json:"preview"`
+}
+
+type TracksResponse struct {
+	Data []Track `json:"data"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("view/*.html")
+	router.Static("/styles", "./view/styles")
+
+	router.GET("/", handleRequest)
+
+	log.Fatal(router.Run(":8080"))
+}
+
+func handleRequest(c *gin.Context) {
+	// Retrieve data from Deezer API
+	response, err := http.Get("https://api.deezer.com/chart/0/tracks")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	var tracksResponse TracksResponse
+	err = json.NewDecoder(response.Body).Decode(&tracksResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Render data in an HTML table
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"Tracks": tracksResponse.Data,
+	})
+}
